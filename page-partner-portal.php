8c58782299038c0110900395e4fa003dbb624c21
deviant96 <mcrealdeviant@gmail.com>
2025-05-28 00:51:12 +0700

    Fix favicon in partner portal

--- page-partner-portal.php
+++ page-partner-portal.php
@@ -11,6 +11,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" sizes="32x32" />
     <?php wp_head(); ?>
     <style>
         input {
